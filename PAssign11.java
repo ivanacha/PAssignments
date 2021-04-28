91024a1cf2456798aef2dbde7636eb6c0f32d530
Ivan C. Acha <achacedric@gmail.com>
2021-04-28 11:19:10 -0400

    PAssign11 added

--- PAssign11.java
+++ PAssign11.java
@@ -1,17 +1,28 @@
-package PAssign;
 
-import java.io.*;
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.File;
+import java.io.IOException;
+import java.io.RandomAccessFile;
 import java.util.ArrayList;
 import java.util.Arrays;
 
+
 public class PAssign11 {
 	
+	// Specify the size of data fields in the record.
+	private final static int EMPLOYEE_NUMBER = 10;
+	private final static int SALARY_SIZE = 8;
+	private final static int YEARS_OF_SERVICE = 4;
+	private final static int RECORD_SIZE = EMPLOYEE_NUMBER + SALARY_SIZE + YEARS_OF_SERVICE;
+
+	
+
 
 	public static void main(String[] args) {
 		try (
-				FileOutputStream output = new FileOutputStream(new File("output.dat"));
 				RandomAccessFile raf = new RandomAccessFile(new File("employeeData.dat"), "rw");
-		) {
+				) {
 			
 			ArrayList<Employee> employees = new ArrayList<>();
 			employees.addAll(Arrays.asList(new Employee("Steve", 27000, 3), new Employee("Floyd", 34050, 6)));
@@ -19,7 +30,7 @@ public class PAssign11 {
 			for (int i =  0; i < employees.size(); i++) {
 				System.out.println(employees.get(i).getInfo());
 			}
-			System.out.printf("Total Salary Before: $%,.2f\n", Employee.companySalaries);
+			System.out.printf("Total Salary Before: $%,.2f", Employee.companySalaries);
 			System.out.println("raising salaries...");
 			for (int i =  0; i < employees.size(); i++) {
 				employees.get(i).processRaise();
@@ -27,43 +38,138 @@ public class PAssign11 {
 			}
 			System.out.printf("Total Salary after: $%,.2f", Employee.companySalaries);
 
+			double value;
+			raf.seek(0);;
+			while ((value = raf.read()) != -1) {
+
+			}
+				
 			
 			
 			
 		} catch (IOException ex) {
-			
+			ex.printStackTrace();
 		}
 
 	}
 	
-	/** Read a record at the specified position */
+	/** Read and write a record at the specified position */
 	
-	public static String readString(int size, DataInput in) throws IOException {
-		char[] chars = new char[size];
+	public static void readEmployeeInfo(int size, DataInput in, Employee em) throws IOException {
 
-		// Read fixed number of characters to the array
-		for (int i = 0; i < size; i++)
-			chars[i] = in.readChar();
 
-		return new String(chars);
 	}
 
 	public static void writeString(String s, int size, DataOutput out) throws IOException {
-		char[] chars = new char[size];
-
-		// Fill in string with characters
-		s.getChars(0, Math.min(s.length(), size), chars, 0);
-
-		// Fill in blank characters in the rest of the array
-		for (int i = Math.min(s.length(), size); i < chars.length; i++)
-			chars[i] = ' ';
-
-		// Create and write a new string padded with blank characters
-		out.writeChars(new String(chars));
-	}
-	
-	public static void processRaise() {
 		
 	}
-}
+	
 
+}
+public class Employee {
+	
+	private String name;
+	private double salary;
+	private int yearsOfService;
+	private static int numEmployees = 0;
+	public static double companySalaries = 0;
+	
+	// Getters and setters.
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		if (name.length() > 0) {
+			this.name = name;
+		} else {
+			this.name = " ";
+		}
+	}
+	public double getSalary() {
+		return salary;
+	}
+	public void setSalary(double salary) {
+		if (salary > 0) {
+			this.salary = salary;
+		} else {
+			this.salary = 0;
+		}
+	}
+	public int getYearsOfService() {
+		return yearsOfService;
+	}
+	public void setYearsOfService(int years) {
+		if (years > 0) {
+			this.yearsOfService = years;
+		} else {
+			this.yearsOfService = 0;
+		}
+	}
+	
+	
+	public static int getNumEmployees() {
+		return numEmployees;
+	}
+	public static void setNumEmployees(int numEmployees) {
+		Employee.numEmployees = numEmployees;
+	}
+	// Constructors
+	public Employee(String name, double salary, int years) {
+		setName(name);
+		setSalary(salary);
+		setYearsOfService(years);
+		numEmployees++;
+		companySalaries += salary;
+	}
+	
+	// Class methods.
+	
+	
+	public void processRaise() {
+		
+		companySalaries -= getSalary();
+		
+		 // Raise for employees in respective earning brackets.
+		if (getSalary() > 80000.00) {
+			
+			if (getYearsOfService() > 2) {
+				setSalary(getSalary() * 1.025);
+			} else {
+				setSalary(getSalary() * 1.03);
+			}
+	
+		} else if (getSalary() > 60000.00) {
+
+			if (getYearsOfService() > 5) {
+				setSalary(getSalary() * 1.015);
+			} else {
+				setSalary(getSalary() * 1.0175);
+			}
+			
+		} else if (getSalary() > 30000.00) {
+
+			if (getYearsOfService() > 5) {
+				setSalary(getSalary() * 1.02);
+			} else {
+				setSalary(getSalary() * 1.0225);
+			}
+			
+		} else if (getYearsOfService() > 5) {
+			
+			setSalary(getSalary() * 1.0101);
+		} else {
+			
+			setSalary(getSalary() * 1.0125);
+		}
+		
+		companySalaries += getSalary();
+		
+	}
+	
+	public String getInfo() {
+		return String.format("Employee: %s, making $%,.2f has been working for %d.", getName(),
+				getSalary(), getYearsOfService());
+	}
+	
+
+}
